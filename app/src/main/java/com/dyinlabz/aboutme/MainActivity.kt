bebbfb58dafcdc50afb3bba6b6af7250fd6fb2b4
Samuel Osewa <osewaayomide4@gmail.com>
2021-07-03 23:26:12 +0100

    Simple logic for app

--- app/src/main/java/com/dyinlabz/aboutme/MainActivity.kt
+++ app/src/main/java/com/dyinlabz/aboutme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dyinlabz.aboutme
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.dyinlabz.aboutme.databinding.ActivityMainBinding
 
@@ -12,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fun addNickName(){
+            binding.nicknameText.visibility = View.GONE
+            val name = binding.editNickname.text.toString()
+            binding.nicknameText.text = name
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+
+
+        binding.doneButton.setOnClickListener {
+            addNickName()
+        }
     }
 }
