a77641801c3a8a6479a2d6b73e75467522e6a887
Samuel Osewa <osewaayomide4@gmail.com>
2021-07-03 11:21:19 +0100

    Layout

--- app/src/main/java/com/dyinlabz/aboutme/MainActivity.kt
+++ app/src/main/java/com/dyinlabz/aboutme/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.dyinlabz.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dyinlabz.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
